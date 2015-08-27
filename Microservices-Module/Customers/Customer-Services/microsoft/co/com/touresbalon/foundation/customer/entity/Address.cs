c304b8053b2d144d3b4a39ce5f441e0210f06e76
harcalejo <harcalejo@gmail.com>
2015-08-27 09:29:54 -0500

    Primera carga del modulo de Customer-Service, se incluyend los archivos fuentes y algunos de configuracion

---
+++ Microservices-Module/Customers/Customer-Services/microsoft/co/com/touresbalon/foundation/customer/entity/Address.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+
+namespace Customer_Services.microsoft.co.com.touresbalon.foundation.customer.entity
+{
+    [DataContract]
+    public class Address
+    {
+        [DataMember]
+        public virtual long Id { get; set; }
+        [DataMember]
+        public virtual string street { get; set; }
+        [DataMember]
+        public virtual string state { get; set; }
+        [DataMember]
+        public virtual string zip { get; set; }
+        [DataMember]
+        public virtual string country { get; set; }
+        [DataMember]
+        public virtual string address_type { get; set; }
+        [DataMember]
+        public virtual string city { get; set; }
+    }
+}
