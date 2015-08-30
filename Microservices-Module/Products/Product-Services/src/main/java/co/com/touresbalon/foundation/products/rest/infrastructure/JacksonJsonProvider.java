05d94623da890d1238d036998fc473525298b103
arciniegas88 <arciniegas88@gmail.com>
2015-08-29 19:14:00 -0500

    implementacion componente de excecpciones

---
+++ Microservices-Module/Products/Product-Services/src/main/java/co/com/touresbalon/foundation/products/rest/infrastructure/JacksonJsonProvider.java
@@ -0,0 +1,31 @@
+package co.com.touresbalon.foundation.products.rest.infrastructure;
+
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.ext.Provider;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
+
+
+/**
+ * Created by garciniegas on 29/08/2015.
+ */
+
+@Provider
+@Produces(MediaType.APPLICATION_JSON)
+public class JacksonJsonProvider extends JacksonJaxbJsonProvider {
+
+    public JacksonJsonProvider() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_DEFAULT);
+        mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        super.setMapper(mapper);
+    }
+
+}
