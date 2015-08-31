1b526cfd1228c9b3a0aea6c7cbc887bde685f54d
stoner2015 <stonerjrc@gmail.com>
2015-08-31 01:12:34 -0500

    SpectaclesWS Javier Rodriguez

---
+++ EIS-Module/SpectaclesWS/src/main/java/co/com/touresbalon/foundation/spectacles/dto/MensajeRespuesta.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.com.touresbalon.foundation.spectacles.dto;
+
+/**
+ *
+ * @author nrodriguez
+ */
+public class MensajeRespuesta {
+    
+    private String codigoRespuesta;
+    private String descripcionRespuesta; 
+
+    
+    public String getCodigoRespuesta() {
+        return codigoRespuesta;
+    }
+
+    public void setCodigoRespuesta(String codigoRespuesta) {
+        this.codigoRespuesta = codigoRespuesta;
+    }
+
+    public String getDescripcionRespuesta() {
+        return descripcionRespuesta;
+    }
+
+    public void setDescripcionRespuesta(String descripcionRespuesta) {
+        this.descripcionRespuesta = descripcionRespuesta;
+    }
+
+    
+    
+    
+}
