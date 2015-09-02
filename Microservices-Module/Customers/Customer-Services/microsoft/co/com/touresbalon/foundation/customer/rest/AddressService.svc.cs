0b5b731645fd32ddbba1b95c941d302f04cd0575
harcalejo <harcalejo@gmail.com>
2015-09-02 01:31:50 -0500

    Actualizacion en el manejo de excepciones y etiquetas para el multi thread de servicios

--- Microservices-Module/Customers/Customer-Services/microsoft/co/com/touresbalon/foundation/customer/rest/AddressService.svc.cs
+++ Microservices-Module/Customers/Customer-Services/microsoft/co/com/touresbalon/foundation/customer/rest/AddressService.svc.cs
@@ -1,29 +1,92 @@
 ﻿using Customer_Services.microsoft.co.com.touresbalon.foundation.customer.entity;
 using Customer_Services.microsoft.co.com.touresbalon.foundation.customer.boundary;
-using System;
+using System.ServiceModel;
+using Cross_Cutting.microsoft.co.com.touresbalon.foundation.crosscutting.exception;
+using System.ServiceModel.Web;
+using Cross_Cutting.microsoft.co.com.touresbalon.foundation.crosscutting.entity;
+using System.Net;
 
 namespace Customer_Services.microsoft.co.com.touresbalon.foundation.customer.rest
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "AddressService" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select AddressService.svc or AddressService.svc.cs at the Solution Explorer and start debugging.
+
+    [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Multiple)]
     public class AddressService : IAddressService
     {
         public string addAddress(string customer_id, Address address)
         {
-            AddressBoundary addressBoundary = new AddressBoundary();
-            return addressBoundary.addAddress(customer_id, address);
+            try
+            { 
+                AddressBoundary addressBoundary = new AddressBoundary();
+                return addressBoundary.addAddress(customer_id, address);
+            }
+            catch(BusinessException e)
+            {
+                throw new WebFaultException<GeneralResponse>
+                    (new GeneralResponse { message = e.Message, status = GeneralResponse.STATUS_ERROR,
+                        code = "400" }, HttpStatusCode.BadRequest);
+            }
+            catch (PlatformException e)
+            {
+                throw new WebFaultException<GeneralResponse>
+                    (new GeneralResponse
+                    {
+                        message = e.Message,
+                        status = GeneralResponse.STATUS_ERROR,
+                        code = "500"
+                    }, HttpStatusCode.BadRequest);
+            }
         }
 
         public string deleteAddress(string id, string customer_id)
         {
-            AddressBoundary addressBoundary = new AddressBoundary();
-            return addressBoundary.deleteAddress(id, customer_id);
+            try
+            { 
+                AddressBoundary addressBoundary = new AddressBoundary();
+                return addressBoundary.deleteAddress(id, customer_id);
+            }
+            catch(BusinessException e)
+            {
+                throw new WebFaultException<GeneralResponse>
+                    (new GeneralResponse { message = e.Message, status = GeneralResponse.STATUS_ERROR,
+                        code = "400" }, HttpStatusCode.BadRequest);
+            }
+            catch (PlatformException e)
+            {
+                throw new WebFaultException<GeneralResponse>
+                    (new GeneralResponse
+                    {
+                        message = e.Message,
+                        status = GeneralResponse.STATUS_ERROR,
+                        code = "500"
+                    }, HttpStatusCode.BadRequest);
+            }
         }
 
         public string updateAddress(Address address)
         {
-            AddressBoundary addressBoundary = new AddressBoundary();
-            return addressBoundary.updateAddress(address);
+            try
+            { 
+                AddressBoundary addressBoundary = new AddressBoundary();
+                return addressBoundary.updateAddress(address);
+            }
+            catch(BusinessException e)
+            {
+                throw new WebFaultException<GeneralResponse>
+                    (new GeneralResponse { message = e.Message, status = GeneralResponse.STATUS_ERROR,
+                        code = "400" }, HttpStatusCode.BadRequest);
+            }
+            catch(PlatformException e)
+            {
+                throw new WebFaultException<GeneralResponse>
+                    (new GeneralResponse
+                    {
+                        message = e.Message,
+                        status = GeneralResponse.STATUS_ERROR,
+                        code = "500"
+                    }, HttpStatusCode.BadRequest);
+            }
         }
     }
 }
--- Microservices-Module/Customers/Customer-Services/microsoft/co/com/touresbalon/foundation/customer/rest/CustomerService.svc.cs
+++ Microservices-Module/Customers/Customer-Services/microsoft/co/com/touresbalon/foundation/customer/rest/CustomerService.svc.cs
@@ -6,11 +6,13 @@ using System.Collections.Generic;
 using System.ServiceModel.Web;
 using Cross_Cutting.microsoft.co.com.touresbalon.foundation.crosscutting.entity;
 using System.Net;
+using System.ServiceModel;
 
 namespace Customer_Services.microsoft.co.com.touresbalon.foundation.customer.rest
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CustomerService" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select CustomerService.svc or CustomerService.svc.cs at the Solution Explorer and start debugging.
+    [ServiceBehavior(ConcurrencyMode=ConcurrencyMode.Multiple)]
     public class CustomerService : ICustomerService
     {
         public string createCustomer(Customer customer)
@@ -26,13 +28,42 @@ namespace Customer_Services.microsoft.co.com.touresbalon.foundation.customer.res
                     (new GeneralResponse { message = e.Message, status = GeneralResponse.STATUS_ERROR,
                         code = "400"}, HttpStatusCode.BadRequest);
             }
+            catch (PlatformException e)
+            {
+                throw new WebFaultException<GeneralResponse>
+                    (new GeneralResponse
+                    {
+                        message = e.Message,
+                        status = GeneralResponse.STATUS_ERROR,
+                        code = "500"
+                    }, HttpStatusCode.BadRequest);
+            }
         }
 
         public string deleteCustomer(string id)
         {
-            CustomerBoundary customerBoundary = new CustomerBoundary();
+            try
+            {
+                CustomerBoundary customerBoundary = new CustomerBoundary();
 
-            return customerBoundary.deleteCustomer(id);
+                return customerBoundary.deleteCustomer(id);
+            }
+            catch(BusinessException e)
+            {
+                throw new WebFaultException<GeneralResponse>
+                    (new GeneralResponse { message = e.Message, status = GeneralResponse.STATUS_ERROR,
+                        code = "400" }, HttpStatusCode.BadRequest);
+            }
+            catch (PlatformException e)
+            {
+                throw new WebFaultException<GeneralResponse>
+                    (new GeneralResponse
+                    {
+                        message = e.Message,
+                        status = GeneralResponse.STATUS_ERROR,
+                        code = "500"
+                    }, HttpStatusCode.BadRequest);
+            }
         }
 
         public Customer getCustomer(string id)
@@ -50,23 +81,71 @@ namespace Customer_Services.microsoft.co.com.touresbalon.foundation.customer.res
                     {
                         message = e.Message,
                         status = GeneralResponse.STATUS_ERROR,
-                        code = HttpStatusCode.BadRequest.ToString()
+                        code = "400"
+                    }, HttpStatusCode.BadRequest);
+            }
+            catch (PlatformException e)
+            {
+                throw new WebFaultException<GeneralResponse>
+                    (new GeneralResponse
+                    {
+                        message = e.Message,
+                        status = GeneralResponse.STATUS_ERROR,
+                        code = "500"
                     }, HttpStatusCode.BadRequest);
             }
         }
 
         public IList<Customer> getCustomers(string pagina, string regPagina)
         {
-            CustomerBoundary customerBoundary = new CustomerBoundary();
+            try
+            { 
+                CustomerBoundary customerBoundary = new CustomerBoundary();
 
-            return customerBoundary.getCustomers(Int32.Parse(pagina), Int32.Parse(regPagina));
+                return customerBoundary.getCustomers(Int32.Parse(pagina), Int32.Parse(regPagina));
+            }
+            catch(BusinessException e)
+            {
+                throw new WebFaultException<GeneralResponse>
+                    (new GeneralResponse { message = e.Message, status = GeneralResponse.STATUS_ERROR,
+                        code = "400" }, HttpStatusCode.BadRequest);
+            }
+            catch (PlatformException e)
+            {
+                throw new WebFaultException<GeneralResponse>
+                    (new GeneralResponse
+                    {
+                        message = e.Message,
+                        status = GeneralResponse.STATUS_ERROR,
+                        code = "500"
+                    }, HttpStatusCode.BadRequest);
+            }
         }
 
         public string updateCustomer(Customer customer)
         {
-            CustomerBoundary customerBoundary = new CustomerBoundary();
+            try
+            { 
+                CustomerBoundary customerBoundary = new CustomerBoundary();
 
-            return customerBoundary.updateCustomer(customer);
+                return customerBoundary.updateCustomer(customer);
+            }
+            catch(BusinessException e)
+            {
+                throw new WebFaultException<GeneralResponse>
+                    (new GeneralResponse { message = e.Message, status = GeneralResponse.STATUS_ERROR,
+                        code = "400" }, HttpStatusCode.BadRequest);
+            }
+            catch (PlatformException e)
+            {
+                throw new WebFaultException<GeneralResponse>
+                    (new GeneralResponse
+                    {
+                        message = e.Message,
+                        status = GeneralResponse.STATUS_ERROR,
+                        code = "500"
+                    }, HttpStatusCode.BadRequest);
+            }
         }
     }
 }
