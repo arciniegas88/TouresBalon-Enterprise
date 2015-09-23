9046a839a97957d153174672c6593db2e29f835a
GustavoAdolfo <arciniegas88@gmail.com>
2015-09-22 21:10:44 -0500

    ajustes

--- EIS-Module/CreditCardWS/src/main/java/co/com/touresbalon/foundation/creditcard/dto/Transaction.java
+++ EIS-Module/CreditCardWS/src/main/java/co/com/touresbalon/foundation/creditcard/dto/Transaction.java
@@ -51,7 +51,7 @@ public class Transaction {
 
     @Override
     public String toString() {
-        return "Transaction{" +
+        return "{" +
                 "number='" + number + '\'' +
                 ", amount=" + amount +
                 ", identificationType='" + identificationType + '\'' +
