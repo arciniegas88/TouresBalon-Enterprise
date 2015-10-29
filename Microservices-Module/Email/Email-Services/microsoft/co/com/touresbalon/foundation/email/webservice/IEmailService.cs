13ac9fd66d1466ba8dc8b8e710de8ed2e7dffbaa
stoner2015 <stonerjrc@gmail.com>
2015-10-28 22:55:56 -0500

    Ajuste URL microservicio email
    Ajuste URL microservicio email

--- Microservices-Module/Email/Email-Services/microsoft/co/com/touresbalon/foundation/email/webservice/IEmailService.cs
+++ Microservices-Module/Email/Email-Services/microsoft/co/com/touresbalon/foundation/email/webservice/IEmailService.cs
@@ -9,8 +9,7 @@ using Email_Services.microsoft.co.com.touresbalon.foundation.email.entity;
 namespace Email_Services.microsoft.co.com.touresbalon.foundation.email.webservice
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IEmailService" in both code and config file together.
-    //ServiceContract]
-    [ServiceContract(Namespace = "http://touresbalon.email.com")]
+    [ServiceContract]    
     public interface IEmailService
     {
         [OperationContract]
